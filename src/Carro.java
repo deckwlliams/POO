dce36781295a38c3a8f070f9865d8c1b12d34722
DERICK WILLIAMS <deckprogramador@gmail.com>
2025-11-03 22:00:59 -0300

    Commit

--- src/Carro.java
+++ src/Carro.java
@@ -15,4 +15,31 @@ void exibirAnuncio(){
     System.out.println(cor + " KM:" + km + " R$" + preco);
 
 }
+void simularSeguro(int perfil){
+    if (perfil == 0){
+        System.out.println("Perfil: idoso -1.5%");
+        System.out.println("R$" + preco * 0.015);
+    }
+    else if (perfil == 1){
+        System.out.println("Perfil: Chefe de familia - 2.5%");
+        System.out.println("R$" + preco * 0.025);
+    }
+    else if (perfil == 2){
+        System.out.println("Perfil: Jovem - 14%");
+        System.out.println("R$" + preco * 0.14);
+    }
+    else {
+        System.out.println("Não temos esse perfil");
+    }
+ }
+double calcularIPVA(){
+  double valorIpva;
+   if (ano<= 2002){
+       valorIpva = 0;
+   }else{
+       valorIpva = preco * 0.035;
+   }
+   return valorIpva;
+
+  }
 }
--- src/UaiBiMotors.java
+++ src/UaiBiMotors.java
@@ -9,20 +9,36 @@ public class UaiBiMotors {
         c1.cor = "Azul";
         c1.ano = 2020;
         c1.km = 0;
-        c1.preco = 2000;
+        c1.preco = 50500;
 
         c2.marca = "Chevrolt";
         c2.modelo = "Corsa";
         c2.cor = "Vermelho";
         c2.ano = 2020;
         c2.km = 0;
-        c2.preco = 2000;
+        c2.preco = 23000;
         
-        c1.exibirAnuncio();
-        c2.exibirAnuncio();
-
+//        c1.exibirAnuncio();
+//        c2.exibirAnuncio();
+        c1.simularSeguro(3);
 //        System.out.println("Carro:");
 //        System.out.println(c1.marca + "-" + c1.modelo + "("+c1.ano+")");
 //        System.out.println(c1.cor + " KM:" + c1.km + " R$" + c1.preco);
+    System.out.println("______Calculando iPVA______");
+    double ipvaC1 = c1.calcularIPVA();
+    double ipvaC2 = c2.calcularIPVA();
+    if(ipvaC1 == 0 ) {
+        System.out.println("Veiculo Isento");
     }
+    else{
+        System.out.println("Valor do IPVA "+ ipvaC1);
+    }
+        if(ipvaC2 == 0 ) {
+            System.out.println("Veiculo Isento");
+        }
+        else{
+            System.out.println("Valor do IPVA "+ ipvaC2);
+        }
+    }
+
 }
